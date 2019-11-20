ddfedde7380c739c179ef1ec4e838e926944bbfc
Oleg Zayarniy <olegza@gmail.com>
2019-11-20 22:03:35 +0100

    Building a board

--- src/App.js
+++ src/App.js
@@ -1,44 +1,39 @@
 import React from 'react';
-import arrow from './arrow.svg';
 import { ReactComponent as Arrow } from './arrow.svg'
 import { ReactComponent as Reset } from './reset.svg'
 import './App.scss';
-import _ from 'lodash';
+import Cell from './components/Cell';
+// import _ from 'lodash';
 
 function App() {
-  const COLORS = ['yellow', 'green', 'blue', 'violet', 'pink', 'red']
+  const COLORS = ['yellow', 'green', 'blue', 'violet', 'pink', 'red'];
+  const SIZE = 14;
+  const matrix = new Array(SIZE)
+  .fill(0)
+  .map(_ => new Array(SIZE).fill(0).map(_ => randomNum(COLORS.length)))
 
-  //let cells = document.querySelectorAll('.col');
+  function randomNum(max) {
+    return Math.floor(Math.random() * max)
+  }
 
-  //document.body.className = COLORS[Math.floor(Math.random() * COLORS.length)];
-
-  //for (let cell of cells) {
-  //  cell.classList.add(COLORS[Math.floor(Math.random() * COLORS.length)])
-  //}
-  //
-  //TODO: make it general and proper size, and put random color here
-  // you will probably need lodash range for it
-  const cells = [0, 1, 2, 3, 4].map(() => (
-    <div className="row">
-      {
-        [0, 1, 2, 3, 4].map(() => (
-          <div className="col blue"></div>
-        ))
-      }
-    </div>
-  ))
   return (
-<div>
-  <div className="top">
-    <div><Arrow/>{"Start here"}</div>
-    <div>{"1/25"}<Reset/></div>
-  </div>
-  <div className="frame">
-    <div className="board">
-     {cells}
+    <div>
+      <div className="top">
+        <div><Arrow/>{"Start here"}</div>
+        <div>{"1/25"}<Reset/></div>
+      </div>
+      <div className="frame">
+        <div className="board">
+          {matrix.map((row, index) => (
+            <div key={index} className="row">
+              {row.map((value, index) => (
+                <Cell key={index} className={COLORS[value]} />
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
-  </div>
-</div>
   )
 }
 
---
+++ src/components/Cell.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Cell({className}) {
+  return (
+    <div className={`col ${className}`} />
+  )
+}
