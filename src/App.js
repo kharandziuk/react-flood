79089655158965dc0ac3514272d5b702c5e4768c
Max Kharandziuk <max.kharandziuk@gmail.com>
2019-11-21 11:00:07 +0100

    add redux

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { ReactComponent as Arrow } from './arrow.svg'
 import { ReactComponent as Reset } from './reset.svg'
+import { shallowEqual, useSelector } from 'react-redux'
 
 function Cell({color}) {
   return (
@@ -9,20 +10,14 @@ function Cell({color}) {
 }
 
 function App() {
-
-  const COLORS = ['yellow', 'green', 'blue', 'violet', 'pink', 'red'];
-  const SIZE = 14;
-
-  function randomNum(max) {
-    return Math.floor(Math.random() * max)
-  }
-
-  const initial = new Array(SIZE)
-  .fill(0)
-  .map(_ => new Array(SIZE).fill(0).map(_ => randomNum(COLORS.length)))
-
-  const [board, changeBoard] = useState(initial);
-
+  let board = useSelector(state => state.field)
+  board = board.map((row, index) => (
+    <div key={index} className="row">
+      {row.map((color, index) => (
+        <Cell key={index} color={color} />
+      ))}
+    </div>
+  ))
   return (
     <>
       <div className="top">
@@ -31,13 +26,7 @@ function App() {
       </div>
       <div className="frame">
         <div className="board">
-          {board.map((row, index) => (
-            <div key={index} className="row">
-              {row.map((value, index) => (
-                <Cell key={index} color={COLORS[value]} />
-              ))}
-            </div>
-          ))}
+          {board}
         </div>
       </div>
     </>
--- src/index.js
+++ src/index.js
@@ -3,18 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux'
+import store from './redux/store'
 
-const COLORS = ['yellow', 'green', 'blue', 'violet', 'pink', 'red']
-
-let cells = document.querySelectorAll('.col');
-
-document.body.className = COLORS[Math.floor(Math.random() * COLORS.length)];
-
-for (let cell of cells) {
-  cell.classList.add(COLORS[Math.floor(Math.random() * COLORS.length)])
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+      <App/>
+    </Provider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
