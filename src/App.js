6fb561ce253e36f729ae12c06ecc9e831ac1c605
Max Kharandziuk <max.kharandziuk@gmail.com>
2019-11-25 23:13:43 +0100

    prototype of a modal

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { ReactComponent as Arrow } from './arrow.svg'
 import { ReactComponent as Reset } from './reset.svg'
 import { useSelector, useDispatch } from 'react-redux'
+import {
+  FacebookShareButton,
+  FacebookIcon,
+  TwitterIcon
+} from 'react-share';
 
 function Cell({color, onClick}) {
   return (
@@ -9,10 +14,25 @@ function Cell({color, onClick}) {
   )
 }
 
+const Modal = ({result}) => {
+  return (<div id="open-modal" className="modal-window">
+    <div>
+      {`You result is ${result}/25`}
+      <FacebookShareButton
+      url={"http://flood-game.s3-website.eu-central-1.amazonaws.com/"}>
+        <FacebookIcon round={true} />
+        <TwitterIcon round={true} />
+      </FacebookShareButton>
+    </div>
+  </div>)
+}
+
 function App() {
   let board = useSelector(state => state.field)
   let counter = useSelector(state => state.counter)
+  let isWin = useSelector(state => state.isWin)
   let currentColor = useSelector(state => state.currentColor)
+  const modal = isWin ?  <Modal result={counter} /> : null
   const dispatch = useDispatch()
   document.body.className = currentColor
   board = board.map((row, index) => (
@@ -25,6 +45,7 @@ function App() {
   ))
   return (
     <>
+      {modal}
       <div className="top">
         <div><Arrow/>{"Start here"}</div>
         <div>{`${counter + 1}/25`}<Reset onClick={()=> dispatch({type: "reset"})}/></div>
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@ import { createStore } from "redux";
 import { flood } from '../utils'
 
 const COLORS = ['yellow', 'green', 'blue', 'violet', 'pink', 'red']
-const SIZE = 20
+const SIZE = 15
 
 function randomNum(max) {
   return Math.floor(Math.random() * max)
@@ -12,6 +12,17 @@ const getField = () => new Array(SIZE)
   .fill(0)
   .map(_ => new Array(SIZE).fill(0).map(_ => COLORS[randomNum(COLORS.length)]))
 
+const isFlooded = (field) => {
+  const color = field[0][0]
+  for(let row of field) {
+    for(let cell of row) {
+      if (cell !== color) {
+        return false
+      }
+    }
+  }
+  return true
+}
 
 const getInitialState = () => {
   const field = getField()
@@ -19,6 +30,7 @@ const getInitialState = () => {
     field,
     counter: 0,
     currentColor: field[0][0],
+    isWin: false
   }
 }
 
@@ -26,12 +38,13 @@ const getInitialState = () => {
 const rootReducer = function(state = getInitialState(), action) {
   switch(action.type) {
     case "flood":
-      if (state.currentColor === action.color) {
+      if (state.isWin || state.currentColor === action.color) {
         return state
       }
       state.field = flood(state.field, action.color)
       state.counter = state.counter + 1
       state.currentColor = action.color
+      state.isWin = isFlooded(state.field)
       return state
     case "reset":
       state = getInitialState()
